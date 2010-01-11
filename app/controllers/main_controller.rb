7e27fe94c62cff742f3b310c46adc48ad8ce11c1
kabob <kabob@smokey>
2010-01-10 19:34:05 -0600

    - added actions to main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,12 @@ class MainController < ApplicationController
   def index
     @post = Post.find(:first, :conditions => ["shown = ?", true], :order => "created_at desc")
   end
+  
+  def manage
+    
+  end
+  
+  def contact
+    
+  end
 end
