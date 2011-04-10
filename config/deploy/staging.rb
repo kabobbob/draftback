cf0bab35d027d071607189523836a5841b85a9cd
Bob McClellan Jr <bob.mcclellan@gmail.com>
2011-04-09 23:36:01 -0500

    Updated deploy scripts for server side rvm

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,16 +3,21 @@ set :repository,  "git@t-rac:draftback.git"
 set :rails_env, 'staging'
 set :scm, :git
 set :user, 'www'
+set :rvm_ruby_string, "ruby-1.8.7-p334@#{application}"
 set :deploy_to, "/home/#{user}/#{application}"
 set :deploy_via, :copy
-set :use_sudo, false
 
 role :web, '174.143.244.245'
 role :app, '174.143.244.245'
 
+before "deploy:update", "config:passenger_stop"
 after "deploy:update", "config:symlink", "config:bundle"
 
 namespace 'config' do
+  task :passenger_stop do
+    run "cd #{current_path} && passenger stop --pid-file tmp/pids/passenger.pid"
+  end
+
   task :symlink do
     run "ln -nfs #{current_path}/public /home/www/www-home/current/public/#{application}"
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
@@ -33,6 +38,6 @@ namespace :deploy do
 
   desc 'restart the app'
   task :restart, :roles => :app do
-    run "touch #{release_path}/tmp/restart.txt"
+    run "cd #{current_path} && passenger start --socket /tmp/#{application}.local.socket -e #{rails_env} -d"
   end
 end
